4969ea4da8bdf0b9ce272119f7e267f31d6ede77
yanwen0614 <yanwen0614@gmail.com>
2017-11-24 15:50:13 +0800

    添加任务说明 连续定位 swith开关等

--- js/utils/collector.js
+++ js/utils/collector.js
@@ -52,6 +52,19 @@ function addUserMarker(lat,lng) {
     mapLayers.addOverlay(marker);
 }
 
+//初始化加载地图
+function mapInit(){
+    //C_BLANK_MAP
+    mapLayers = new CMapLayers('map', { mapTypes: [TDTVECTORMAP,TDTMIXMAP], center: new CLatLng(39.9,116.37), zoom: 11, doubleClickZoom: false });
+    mapLayers.addLatLngCtl();
+    CEvent.addListener(mapLayers,"click",function(){
+        $("#m_close").click();
+    });
+    //addUserMarker();
+    
+    addLocationUserMarker();
+}
+
 function checkcollectioninfo(rawdata)
 {
     var data = rawdata.serializeArray()
@@ -95,12 +108,6 @@ function clearFormInfo(clnName) {
         }
     }
 
-function unlogin(){
-    Cookies.set('hasbeenlogin',false)
-    Cookies.set('isAutologin',false)
-    location.replace('./login.html')
-}
-
 function checklogin() {
     var hasbeenlogin = eval(Cookies.get('hasbeenlogin'));
     readCollectorInfo4Cookies()
@@ -108,6 +115,8 @@ function checklogin() {
 }
     
 function displaymenu(idstr){
+    $("#menu_on").hide()
+    $("#menu_setting").hide()
     $(idstr).show()
     $(idstr).animate({bottom:'0px'},function(){
     });
@@ -117,4 +126,71 @@ function displaymenu(idstr){
     });
     $(".left_list").animate({bottom:'65%'});
     $(".right_list").animate({bottom:'65%'});
+}
+
+function unlogin(){
+    var r=confirm("确认退出登陆");
+    if (r==true)
+    {
+        Cookies.set('hasbeenlogin',false);
+        Cookies.set('isAutologin',false);
+        location.replace('./login.html');
+    }
+}
+
+function gettask(){
+    // to do
+    var tasklist = new Array
+for (let i=65;i<91;i++) {
+    tasklist.push(String.fromCharCode(i))
+}
+for(let i = 0;i < tasklist.length; i++){
+    var rand = parseInt(Math.random()*tasklist.length);
+       t = tasklist[rand];
+       tasklist[rand] =tasklist[i];
+       tasklist[i] = t;
+  }
+  return tasklist.slice(0,3)
+}
+
+function loadtask() {
+
+    var task = gettask(); 
+    list = $('#tasklist');
+    len = $('li#task').length
+    for (let i = 0; i <len ; i++) {
+        $('li#task').get(0).remove()
+    }
+   
+    for (let i = 0; i < task.length; i++) {
+        const element = task[i];
+        list.append("<li id='task'>" +element+"</li>")
+        
+    }
+}
+
+function settingInit() {
+    var isAutologin= eval(Cookies.get('isAutologin'));
+    var isAutolocated = eval(Cookies.get('isAutolocated'));
+    $("#AutoLoginSwitch").get(0).checked = isAutologin;
+    $("#AutoLocatedSwitch").get(0).checked = isAutolocated;
+    $('#unlogin').click(unlogin);
+    loadtask();
+    $("#AutoLoginSwitch").click(function() {  
+        if ($("#AutoLoginSwitch").is(':checked')) {  
+            Cookies.set('isAutologin',true);  
+        } else {  
+            Cookies.set('isAutologin',false);  
+        }  
+    })
+    var t
+    $("#AutoLocatedSwitch").click(function() {  
+        if ($("#AutoLocatedSwitch").is(':checked')) {  
+            t = setInterval('addLocationUserMarker()',2e3);  
+        } else {  
+            t=window.clearInterval(t);  
+        }  
+    })
+    $('#refreshTask').click(loadtask)
+
 }
